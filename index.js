93121a9fda15bf91aec119bd4eb665f49b5d9fc4
ashish-patwal <ashishpatwal147@gmail.com>
2021-07-22 16:54:15 +0530

    new index.js

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
 // chrome://extensions/
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+let leads = getLeads();
+
+if (leads == null) {
+    setLeads([]);
 }
+
+render();
 
 tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        addLead(tabs[0].url)
         render(myLeads)
     })
 })
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
-
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
+    setLeads([])
     myLeads = []
-    render(myLeads)
+    render()
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+    addLead(inputEl.value);
+    inputEl.value = "";
+    render();
 })
+
+function render() {
+    let leads = getLeads();
+    let listItems = ""
+    leads.forEach( lead => {        
+    listItems += `
+            <li>
+                <a target='_blank' href='${lead}'>
+                    ${lead}
+                </a>
+                <button onclick="clearThisLead('${lead}')">Remove</button>
+            </li>
+        `
+    })
+    ulEl.innerHTML = listItems
+}
+
+function getLeads() {
+    leadsFromLocalStorage = localStorage.getItem("myLeads");
+    if(leadsFromLocalStorage) {
+        return JSON.parse(leadsFromLocalStorage);
+    }
+    return null;
+}
+
+function setLeads(leads) {
+    return localStorage.setItem('myLeads', JSON.stringify(leads));
+}
+
+function removeLead(lead) {
+    let leads = getLeads();
+    let index = leads.indexOf(lead);
+    if (index > -1) {
+        leads.splice(index, 1);
+    }
+    setLeads(leads);
+}
+
+function addLead(lead) {
+    let leads = getLeads();
+    leads.push(lead);
+    setLeads(leads);
+}
+
+function clearThisLead(lead) {
+    removeLead(lead);
+    render();
+}
